af39436d4d223fdaaf08b2f0ab16e657b660885c
Ahmad Hijazi <ahmadhijazi_me@hotmail.com>
2022-04-03 13:13:26 +0200

    master

--- question_module.py
+++ question_module.py
@@ -1,5 +1,11 @@
 class Question:
-    def __init__(self, text, answer):
-        self.text = text
-        self.answer = answer
+    def __init__(self, q_category, q_type, q_difficulty, q_question, q_correct_answer, q_incorrect_answers):
+        self.q_category = q_category
+        self.q_type = q_type
+        self.q_difficulty = q_difficulty
+        self.q_question = q_question
+        self.q_correct_answer = q_correct_answer
+        self.q_incorrect_answers = q_incorrect_answers
+
+
 
